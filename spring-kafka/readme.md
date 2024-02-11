3d415e9826764c224d004dfe83d4ba7c15e8ddcf
chanqun <cksgns93@naver.com>
2024-02-11 13:17:27 +0900

    routing replying template

--- spring-kafka/readme.md
+++ spring-kafka/readme.md
@@ -61,3 +61,42 @@ public ApplicationRunner runner(AdminClient adminClient) {
 }
 ```
 
+
+## 1. kafkaTemplate 설정
+
+a. ProducerFactory 클래스를 이용해 생성
+ - 트랜잭션을 사용하지 않는 경우, Singleton 으로 생성
+ - flush()를 사용할 경우 같은 Porudcer를 사용하는 다른 쓰레드에서 지연현상이 발생할 수 있음
+   - 2.3 이후부터 producerPerThread속성 추가
+   - true로 설정하게 되면, 각 쓰레드에서 별도의 생성자를 만들고 캐시처리
+   - 생성자가 더이상 필요하지 않을 경우 closeThreadBoundProducer()를 호출
+ - 2.5.10 부터는 설정을 업데에트하거나 제거할 수 있는 method를 제공
+   - SSL 키 변경등에 유용하게 사용
+   - reset()을 사용할 경우, 기존 Producer 는 닫고 새로운 설정으로 Producer 생성
+   - Transactional, Non-Transactional 로의 변경은 불가능
+
+
+1. KafkaTemplate 메시지 발송
+- 기본적으로 비동기처리
+    - 동기로 처리할 수 있으나 카프카의 목적이 빠른 스트림 처리이므로 사용하지 않는게 좋음
+- 발송방법
+    - Message<?>객체를 이용
+        - 메시지에 헤더로 정보를 제공 가능 (TOPIC, PARTITION_ID, MESSAGE_KEY, TIMESTAMP)
+    - ProducerRecord<K, V>를 이용
+    - 토픽, 파티션, 오프셋 설정 후 전송
+2. KakfaTemplate Listener
+- KafkaSendCallback
+
+3. RoutingKafkaTemplate
+전송하는 토픽별로 옵션을 다르게 설정할 수 있음
+transactions, execute, flust, metric 사용할 수 없음
+
+4. ReplyingKafkaTemplate
+Consumer 특정 데이터 전발 여부 확인 할 수 있음
+KafkaHeaders. CORRELATION_ID, REPLY_TOPIC, REPLY_PARTITION
+
+
+
+
+ 
+
--- spring-kafka/src/main/java/com/example/springkafka/SpringKafkaApplication.java
+++ spring-kafka/src/main/java/com/example/springkafka/SpringKafkaApplication.java
@@ -1,5 +1,6 @@
 package com.example.springkafka;
 
+import com.example.springkafka.producer.ClipProducer;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.TopicDescription;
 import org.apache.kafka.clients.admin.TopicListing;
@@ -7,7 +8,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
@@ -19,20 +23,11 @@ public class SpringKafkaApplication {
     }
 
     @Bean
-    public ApplicationRunner runner(AdminClient adminClient) {
+    public ApplicationRunner runner(ClipProducer clipProducer) {
         return args -> {
-            Map<String, TopicListing> topics = adminClient.listTopics().namesToListings().get();
-
-            for (String topicName : topics.keySet()) {
-                TopicListing topicListing = topics.get(topicName);
-                System.out.println(topicListing);
-
-                Map<String, TopicDescription> description = adminClient.describeTopics(Collections.singleton(topicName)).allTopicNames().get();
-
-                System.out.println(description);
-
-                adminClient.deleteTopics(Collections.singleton(topicName));
-            }
+            clipProducer.async("clip3", "hello, async");
+            clipProducer.sync("clip3", "hello, sync");
+            Thread.sleep(3000);
         };
     }
 }
